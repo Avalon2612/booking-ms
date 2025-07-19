f030abb9165ca101370960917df729b5c7210edc
Avalon2612 <abhijeetsarode3103@gmail.com>
2025-07-19 14:54:07 +0530

    add Jenkinsfile

---
+++ src/main/java/com/example/booking_ms/train.java
@@ -0,0 +1,12 @@
+package com.example.booking_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class train   {
+    @GetMapping("/train")
+    public String getData() {
+        return "Please  book train ticket with 30% discount";
+    }
+}
